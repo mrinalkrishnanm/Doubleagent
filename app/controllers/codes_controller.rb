9efa7b6e7aeedab60a9a304891a003880d0734ef
Akhil Ramesh <akkhil@outlook.com>
2016-07-07 20:27:06 +0530

    modified search logic

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -1,42 +1,42 @@
 class CodesController < ApplicationController
-	skip_before_filter :verify_authenticity_token
-	
-	def index
-		@code = Code.all
-		render json: @code,status: 200
-	end
-    
-    def search
-    	if params[:keyword].present?
-    		@code = Code.where("language=? AND difficulty=? AND name LIKE ?",params[:language],params[:difficulty],"%#{params[:keyword]}%") 
-        else 
-        	@code = Code.where("language=? AND difficulty=?",params[:language],params[:difficulty]) 	
-        end
-        render json: @code,status: 200
-    end
+  skip_before_filter :verify_authenticity_token
 
-    def create
-    	@code = Code.new(code_params)
-    	if @code.save
-    		render json: @code,status: 200
-    	else
-    		render @code.error,status: 422
-    	end
-    end
+  def index
+    @code = Code.all
+    render json: @code,status: 200
+  end
 
-    def edit
+  def search
+    if params[:keyword].present? && params[:language].present?
+      @code = Code.where("language=? AND name LIKE ?",params[:language],"%#{params[:keyword]}%")
+    else
+      @code = Code.where("language=? OR name LIKE ?",params[:keyword],params[:keyword])
     end
+    render json: @code, status: 200
+  end
 
-    def update
+  def create
+    @code = Code.new(code_params)
+    if @code.save
+      render json: @code,status: 200
+    else
+      render @code.error,status: 422
     end
+  end
 
-    def destroy 
-    end
+  def edit
+  end
 
-    private
-        def code_params
-            params.require(:code).permit(:name,:language,:difficulty,:app_link,:github_link,:description, :category)
-        end
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def code_params
+    params.require(:code).permit(:name,:language,:difficulty,:app_link,:github_link,:description, :category)
+  end
 end
 
-# kata, web, mobile, snippet 
+# kata, web, mobile, snippet
