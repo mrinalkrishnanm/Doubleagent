ae2bebd8d43f8d5eb9d50ffc2b1db1ea4db9f628
Akhil Ramesh <akkhil@outlook.com>
2016-07-19 01:47:42 +0530

    some debug

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,25 +1,27 @@
 class TokensController < ApplicationController
-	def verify
-		@user = User.find_by(username: params[:user][:username])
-        @flag = @user.valid_password?(params[:user][:password])
+  def verify
+    @user = User.find_by(username: params[:user][:username])
+    @flag = @user.valid_password?(params[:user][:password])
+    logger.debug(@flag)
+    logger.debug(params)
 
 
-    	if @flag
-    		@token = @user.access_token
-    		logger.debug(@token)
-    		render json: {token: @token}, status: 200
-    	else
-    		render status: 422
-        end
+    if @flag
+      @token = @user.access_token
+      logger.debug(@token)
+      render json: {token: @token}, status: 200
+    else
+      render status: 422
     end
+  end
 
 
-    def verify_token
-    	@user = User.find_by(access_token: params[:token])
-    	if @user
-          render json: @user, status: 200
-     	else
-        	render json: @user.errors, status: 422
-      end
+  def verify_token
+    @user = User.find_by(access_token: params[:token])
+    if @user
+      render json: @user, status: 200
+    else
+      render json: @user.errors, status: 422
     end
+  end
 end
