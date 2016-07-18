660130b4782242039ed005374f7c9ea8d9bef20d
Akhil Ramesh <akkhil@outlook.com>
2016-07-19 01:20:53 +0530

    some more changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,34 @@
 class UsersController < ApplicationController
-	def index
-		@users=User.all
-		render json: @users, status: 200
-	end
+  def index
+    @users=User.all
+    render json: @users, status: 200
+  end
 
-    def show
-    	@user=User.find(params[:id])
-    	render json: @users, status: 200
+  def show
+    @user=User.find(params[:id])
+    render json: @users, status: 200
+  end
+
+  def create
+    @user=User.new(user_params)
+    if @user.save
+      render json: @user, status: 200
+    else
+      render @user.error, status: 422
     end
+  end
 
-    def create
-    	@user=User.new(user_params)
-    	if @user.save
-    		render json: @user, status: 200
-    	else
-    		render @user.error, status: 422
-    	end
-    end
+  def edit
+  end
 
-    def edit
-    end
+  def update
+  end
 
-    def update
-    end
+  def destroy
+  end
 
-    def destroy
-    end
-
-    private
-        def user_params
-            params.require(:user).permit(:first_name,:last_name,:username,:email,:password);
-        end
+  private
+  def user_params
+    params.require(:user).permit(:first_name,:last_name,:username,:email,:password);
+  end
 end
